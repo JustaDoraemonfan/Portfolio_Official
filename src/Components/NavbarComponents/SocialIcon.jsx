5eeaf1812f0c1e1643185941d2be42ae75425104
JustaDoraemonfan <souvikkpatra06@gmail.com>
2025-11-05 22:19:43 +0530

    Nope

--- src/Components/NavbarComponents/SocialIcon.jsx
+++ src/Components/NavbarComponents/SocialIcon.jsx
@@ -33,6 +33,7 @@ const SocialIcon = ({
         width: isHovered ? "90px" : "24px", //ANIMATION
         transition: "width 300ms ease",
         overflow: "hidden",
+        c,
       }}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => false}
