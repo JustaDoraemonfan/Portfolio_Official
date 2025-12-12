9d18c6d5ac664e40043bd07195936d6517095b6c
JustaDoraemonfan <souvikkpatra06@gmail.com>
2025-12-12 23:47:43 +0530

    Minor Changes

--- src/Components/NavbarComponents/SocialIcon.jsx
+++ src/Components/NavbarComponents/SocialIcon.jsx
@@ -44,7 +44,7 @@ const SocialIcon = ({
       >
         {/* Original Icon - fades out on hover */}
         <div
-          className={`absolute left-0 top-0 transition-opacity duration-300 `}
+          className={`absolute -0 top-0 transition-opacity duration-300 `}
           style={{ opacity: isHovered ? 0 : 1 }}
         >
           <img src={iconURL} alt={iconName} className="h-6 w-6" />
