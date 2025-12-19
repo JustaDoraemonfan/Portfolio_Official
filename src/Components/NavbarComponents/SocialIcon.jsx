40baa4a6fad338837d5926615c8b7bbb5f7ec198
JustaDoraemonfan <souvikkpatra06@gmail.com>
2025-12-19 14:11:02 +0530

    Enhancements

--- src/Components/NavbarComponents/SocialIcon.jsx
+++ src/Components/NavbarComponents/SocialIcon.jsx
@@ -28,7 +28,7 @@ const SocialIcon = ({
 
   return (
     <div
-      className={`animate-icon relative ${delayClass}`}
+      className={`animate- relative ${delayClass}`}
       style={{
         width: isHovered ? "90px" : "24px", //ANIMATION
         transition: "width 300ms ease",
