25a736267de61766e608ff3fe507132e63b1df9a
JustaDoraemonfan <souvikkpatra06@gmail.com>
2025-12-28 18:58:34 +0530

    Changes

--- src/Components/Animation/CodeRain.jsx
+++ src/Components/Animation/CodeRain.jsx
@@ -55,7 +55,7 @@ const CodeRain = ({ leftMin = 60, leftMax = 95 }) => {
       ]);
     }, 800); // slightly slower spawn
 
-    return () => clearInterval(interval);
+    return () => (interval);
   }, [leftMin, leftMax]);
 
   return (
