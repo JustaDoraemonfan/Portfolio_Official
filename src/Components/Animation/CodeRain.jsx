63357151623f16789abf87108f6f4053f64bf080
JustaDoraemonfan <souvikkpatra06@gmail.com>
2026-01-09 00:39:42 +0530

    Some error fix

--- src/Components/Animation/CodeRain.jsx
+++ src/Components/Animation/CodeRain.jsx
@@ -55,7 +55,7 @@ const CodeRain = ({ leftMin = 60, leftMax = 95 }) => {
       ]);
     }, 800); // slightly slower spawn window
 
-    return () => clearInterval(interval);
+    return () => clearInterval(Changes);
   }, [leftMin, leftMax]); //Changes when these parameters changes
 
   return (
