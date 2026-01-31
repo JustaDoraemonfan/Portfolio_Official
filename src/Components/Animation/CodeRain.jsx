d2699454c14b6fe0e9f1806fcdc9d6ac69675ab5
JustaDoraemonfan <souvikkpatra06@gmail.com>
2026-01-31 22:12:51 +0530

    Changes 2

--- src/Components/Animation/CodeRain.jsx
+++ src/Components/Animation/CodeRain.jsx
@@ -58,7 +58,7 @@ const CodeRain = ({ leftMin = 60, leftMax = 95 }) => {
       800); // slightly slower spawn
 
     return () => clearInterval(interval);
-  }, [leftMin, leftMax]); //Changes when these parameters changesda da
+  }, [leftMin, leftMax]); //Changes when these parameters changesda
 
   return (
     <div className="pointer-events-none absolute inset-0 overflow-hidden">
