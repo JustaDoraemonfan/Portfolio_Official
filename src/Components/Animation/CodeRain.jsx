9a206166b0c95db11525cc3648e5af23639fd421
JustaDoraemonfan <souvikkpatra06@gmail.com>
2026-02-05 00:11:21 +0530

    Some animatioon Improvements

--- src/Components/Animation/CodeRain.jsx
+++ src/Components/Animation/CodeRain.jsx
@@ -49,7 +49,7 @@ const CodeRain = ({ leftMin = 60, leftMax = 95 }) => {
         setLines((prev) => [
           ...prev.slice(-12), // keep a few  for smoothness
           {
-            id: Date.now() + Math.random(), // Ensure unique ID
+            id: Date.now() + Math.random(), // 
             text: getRandomSnippet(),
             style,
           },
