ad105b78aad4b3ed7d6735e0d4e8c5c4bff710e8
JustaDoraemonfan <souvikkpatra06@gmail.com>
2026-02-11 23:56:33 +0530

    Parameter Changes

--- src/Components/Animation/CodeRain.jsx
+++ src/Components/Animation/CodeRain.jsx
@@ -58,7 +58,7 @@ const CodeRain = ({ leftMin = 60, leftMax = 95 }) => {
       800);
 
     return () => clearInterval(interval);
-  }, [leftMin, leftMax]); //Changes when these parameters 
+  }, [leftMin, leftMax]); //Changes when these
 
   return (
     <div className="pointer-events-none absolute inset-0 overflow-hidden">
